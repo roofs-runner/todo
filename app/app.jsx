13352cb01e0c9d2a4923012693636c9d26000118
Sergio <sergio@Sergios-MacBook-Pro.local>
2017-05-30 09:43:52 +0300

    Addded todo component

--- app/app.jsx
+++ app/app.jsx
@@ -2,6 +2,8 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 
+var ToDoApp = require ('ToDoApp');
+
 //Include foundation
 require('style!css!foundation-sites/dist/css/foundation.css');
 $(document).foundation();
@@ -10,7 +12,6 @@ $(document).foundation();
 require('style!css!sass!applicationStyles');
 
 ReactDOM.render(
-
-    <p>Boilerplate3</p>,
+    <ToDoApp />,
     document.getElementById('app')
 );
